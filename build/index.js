80db2c83cb0908f4444b73a6383e28d8c96d6526
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-09-11 13:25:23 -0300

    change port config

--- build/index.js
+++ build/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 //app.listen(PORT, ...: This tells the Express application to start listening for incoming HTTP requests on the port specified by PORT
 //() => { ... }: This is a callback function that gets executed once the server starts successfully
 app_1.default.listen(PORT, () => {
