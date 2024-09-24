bdc064ac664773f9ad9962e76d419e1424eccf18
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-09-24 10:20:25 -0300

    add trajectory business logic

---
+++ build/services/trajectoryService.js
@@ -0,0 +1,18 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getTrajectories = void 0;
+const trajectoryRepository_1 = require("../repositories/trajectoryRepository");
+const getTrajectories = (taxiId, date) => __awaiter(void 0, void 0, void 0, function* () {
+    const trajectories = yield (0, trajectoryRepository_1.findTrajectoriesByTaxiAndDate)(taxiId, date);
+    return trajectories;
+});
+exports.getTrajectories = getTrajectories;
