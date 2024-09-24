80f9539b37f75ecc2bca412d8b99bba7b09210af
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-09-24 14:24:50 -0300

    add trajectory query logic

---
+++ build/repositories/trajectoryRepository.js
@@ -0,0 +1,58 @@
+"use strict";
+//querying the db for trajectories that match the taxiId and date
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findTrajectoriesByTaxiAndDate = void 0;
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+const findTrajectoriesByTaxiAndDate = (taxiId, date) => __awaiter(void 0, void 0, void 0, function* () {
+    //parse the incoming date string (format DD-MM-YYYY)
+    const [day, month, year] = date.split('-').map(Number);
+    //checking if the parsed date components are valid
+    if (!day || !month || !year || isNaN(day) || isNaN(month) || isNaN(year)) {
+        throw new Error('Invalid date format');
+    }
+    //creating a Date object for the start of the day
+    const startOfDay = new Date(year, month - 1, day, 0, 0, 0); // month - 1 because months are 0-indexed | 00:00:00
+    //creating a Date object for the end of the day
+    const endOfDay = new Date(year, month - 1, day, 23, 59, 59);
+    //checking if the created Date objects are valid
+    if (isNaN(startOfDay.getTime()) || isNaN(endOfDay.getTime())) {
+        throw new Error('Invalid date');
+    }
+    //querying the db filtering between the start and end of the day
+    const trajectories = yield prisma.trajectories.findMany({
+        where: {
+            taxi_id: parseInt(taxiId),
+            date: {
+                gte: startOfDay, //gte: greater than or equal to (start of the day)
+                lte: endOfDay, //lte: less than or equal to (end of the day)
+            },
+        },
+        select: {
+            id: true,
+            taxi_id: true,
+            date: true,
+            latitude: true,
+            longitude: true,
+        },
+    });
+    //transforming the `taxi_id` to `taxiId` in the returned data
+    const transformedTrajectories = trajectories.map(trajectory => ({
+        id: trajectory.id,
+        taxiId: trajectory.taxi_id, //map `taxi_id` to `taxiId`
+        date: trajectory.date,
+        latitude: trajectory.latitude,
+        longitude: trajectory.longitude,
+    }));
+    return transformedTrajectories;
+});
+exports.findTrajectoriesByTaxiAndDate = findTrajectoriesByTaxiAndDate;
