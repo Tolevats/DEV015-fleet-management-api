194eef0bd326b8d84e8f4623bf3ecaab8b03af93
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-09-24 23:27:57 -0300

    add latest route to trajectories

--- build/routes/trajectoryRoutes.js
+++ build/routes/trajectoryRoutes.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const trajectoryController_1 = require("../controllers/trajectoryController");
 const router = (0, express_1.Router)();
-// Define the GET /trajectories route
+//route for fetching trajectories by taxiId and date
 router.get('/trajectories', trajectoryController_1.fetchTrajectories);
+//route for fetching the latest trajectories
+router.get('/trajectories/latest', trajectoryController_1.fetchLatestTrajectories);
 exports.default = router;
