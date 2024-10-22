020fdf2c7448a1d2a1436013f2106689a3fa013d
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-10-21 21:28:07 -0300

    update: add new authentication route

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 //initializing the Express app and attaching the routes
-
 import express from 'express';
 import taxiRoutes from './routes/taxiRoutes';
 import trajectoryRoutes from './routes/trajectoryRoutes';
 import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json()); //parses incoming request body
 
 //register routes
 app.use(taxiRoutes);
 app.use(trajectoryRoutes);
 app.use(userRoutes);
+app.use(authRoutes);
 
 export default app;
