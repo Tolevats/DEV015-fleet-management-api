5ffb55b7af48d3ebbea417fede26e5b136b47683
Tolevats <142176061+Tolevats@users.noreply.github.com>
2024-10-21 22:17:44 -0300

    update: add new authentication route

--- build/app.js
+++ build/app.js
@@ -1,17 +1,19 @@
 "use strict";
-//initializing the Express app and attaching the routes
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+//initializing the Express app and attaching the routes
 const express_1 = __importDefault(require("express"));
 const taxiRoutes_1 = __importDefault(require("./routes/taxiRoutes"));
 const trajectoryRoutes_1 = __importDefault(require("./routes/trajectoryRoutes"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const app = (0, express_1.default)();
-app.use(express_1.default.json());
+app.use(express_1.default.json()); //parses incoming request body
 //register routes
 app.use(taxiRoutes_1.default);
 app.use(trajectoryRoutes_1.default);
 app.use(userRoutes_1.default);
+app.use(authRoutes_1.default);
 exports.default = app;
